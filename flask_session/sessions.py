25942e857782d672cfd8bdef9a107b00babe0285
Randy Syring <randy.syring@level12.io>
2015-06-11 08:49:23 -0400

    remove call to db.create_all() in SqlAlchemySessionInterface

--- flask_session/sessions.py
+++ flask_session/sessions.py
@@ -455,7 +455,6 @@ class SqlAlchemySessionInterface(SessionInterface):
             def __repr__(self):
                 return '<Session data %s>' % self.data
 
-        self.db.create_all()
         self.sql_session_model = Session
 
     def open_session(self, app, request):
