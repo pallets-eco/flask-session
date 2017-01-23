ed62129fa1d9af36e77e9ce1de571d2c873a6c4e
Kurt Grutzmacher <grutz@jingojango.net>
2017-01-23 13:11:25 -0800

    Adds check to ensure saved_session.expiry exists before utcnow() check. fixes #56

--- flask_session/sessions.py
+++ flask_session/sessions.py
@@ -513,7 +513,7 @@ class SqlAlchemySessionInterface(SessionInterface):
         store_id = self.key_prefix + sid
         saved_session = self.sql_session_model.query.filter_by(
             session_id=store_id).first()
-        if saved_session and saved_session.expiry <= datetime.utcnow():
+        if saved_session and (not saved_session.expiry or saved_session.expiry <= datetime.utcnow()):
             # Delete expired session
             self.db.session.delete(saved_session)
             self.db.session.commit()
