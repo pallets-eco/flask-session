edd3a9caa4d8be85ab15c32f1df4119185b30493
Lex <lxstr@protonmail.com>
2023-12-31 22:36:40 +1100

    Fix expiry is None bug in SQLAlchemy
    Closes #203
    Based on flask-session2 rather than other PRs as the expected behaviour would be to remove the session with no expiry

--- src/flask_session/sessions.py
+++ src/flask_session/sessions.py
@@ -433,7 +433,9 @@ class SqlAlchemySessionInterface(ServerSideSessionInterface):
         store_id = self.key_prefix + sid
         saved_session = self.sql_session_model.query.filter_by(
             session_id=store_id).first()
-        if saved_session and saved_session.expiry <= datetime.utcnow():
+        if saved_session and (
+            not saved_session.expiry or saved_session.expiry <= datetime.utcnow()
+        ):
             # Delete expired session
             self.db.session.delete(saved_session)
             self.db.session.commit()
