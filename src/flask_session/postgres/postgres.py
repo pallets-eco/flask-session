77046f7217b1c73685c1fbbd8b779c4f3bf81ddb
Giuseppe Papallo <giuseppe@papallo.it>
2024-03-28 09:30:36 -0300

    implement abc methods

--- src/flask_session/postgres/postgres.py
+++ src/flask_session/postgres/postgres.py
@@ -1,17 +1,27 @@
 from __future__ import annotations
 
 from typing import Optional
+from contextlib import contextmanager
 
 from flask import Flask
 from psycopg2.pool import ThreadedConnectionPool
+from psycopg2.extensions import connection as PsycoPg2Connection
+from psycopg2.extensions import cursor as PsycoPg2Cursor
 from datetime import timedelta as TimeDelta
+from datetime import datetime
+from typing import Any, Generator, Optional
+from itsdangerous import want_bytes
 
 from ..base import ServerSideSession, ServerSideSessionInterface
 from ..defaults import Defaults
 
+from ._queries import Queries
+from .._utils import retry_query
+
 DEFAULT_TABLE_NAME = "flask_sessions"
 DEFAULT_SCHEMA_NAME = "public"
 DEFAULT_PG_MAX_DB_CONN = 10
+
 
 class PostgreSqlSession(ServerSideSession):
     pass
@@ -22,7 +32,6 @@ class PostgreSqlSessionInterface(ServerSideSessionInterface):
 
     session_class = PostgreSqlSession
     ttl = True
-
 
     def __init__(
         self,
@@ -63,6 +72,8 @@ class PostgreSqlSessionInterface(ServerSideSessionInterface):
         self._table = table_name
         self._schema = schema_name
 
+        self._queries = Queries(schema=schema_name, table=table_name)
+
         super().__init__(
             app,
             key_prefix,
@@ -73,15 +84,73 @@ class PostgreSqlSessionInterface(ServerSideSessionInterface):
             cleanup_n_requests,
         )
 
+        # QUERY HELPERS
+
+        @contextmanager
+        def _get_cursor(
+            self, conn: Optional[PsycoPg2Connection] = None
+        ) -> Generator[PsycoPg2Cursor, None, None]:
+            _conn: PsycoPg2Connection = conn or self.pool.getconn()
+
+            assert isinstance(_conn, PsycoPg2Connection)
+            try:
+                with _conn:
+                    with _conn.cursor() as cur:
+                        yield cur
+            except Exception:
+                raise
+            finally:
+                self.pool.putconn(_conn)
+
+        @retry_query(max_attempts=3)
+        def _create_schema_and_table(self) -> None:
+            with self._get_cursor() as cur:
+                cur.execute(self._queries.create_schema)
+                cur.execute(self._queries.create_table)
+
         def _delete_expired_sessions(self) -> None:
-            raise NotImplementedError
+            """Delete all expired sessions from the database."""
+            with self._get_cursor() as cur:
+                cur.execute(self._queries.delete_expired_sessions)
 
-        def _delete_session(self, store_id:str) -> None:
-            raise NotImplementedError
+        @retry_query(max_attempts=3)
+        def _delete_session(self, store_id: str) -> None:
+            with self._get_cursor() as cur:
+                cur.execute(
+                    self._queries.delete_session,
+                    dict(session_id=self._get_store_id(store_id)),
+                )
 
-        def _upsert_session(self, session_lifetime:TimeDelta, session: ServerSideSession, store_id: str) -> None:
-            raise NotImplementedError
+        @retry_query(max_attempts=3)
+        def _retrieve_session_data(self, store_id: str) -> bytes | None:
+            with self._get_cursor() as cur:
+                cur.execute(
+                    self._queries.retrieve_session_data,
+                    dict(session_id=self._get_store_id(store_id)),
+                )
+                session_data = cur.fetchone()
 
-        def _retrieve_session_data(self, store_id: str)-> Optional[dict]:
-            raise NotImplementedError
+            if session_data is not None:
+                serialized_session_data = want_bytes(session_data)
+                return self.serializer.decode(serialized_session_data)
+            return None
 
+        @retry_query(max_attempts=3)
+        def _upsert_session(
+            self, session_lifetime: TimeDelta, session: ServerSideSession, store_id: str
+        ) -> None:
+
+            serialized_session_data = self.serializer.encode(session)
+
+            # TODO: use database's NOW() rather than datetime.utcnow()
+            expiry = datetime.utcnow() + session_lifetime
+
+            with self._get_cursor() as cur:
+                cur.execute(
+                    self._queries.upsert_session,
+                    dict(
+                        session_id=session.sid,
+                        data=serialized_session_data,
+                        expiry=expiry,
+                    ),
+                )
